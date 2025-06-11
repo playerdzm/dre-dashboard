cba3dae72a688c53c82531ff12f6880893bb8c89
Diego Zanqueta <dzmplayer4@gmail.com>
2025-06-11 01:55:38 -0300

    Atualiza favicon, título, cores e App.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // aqui definimos a sua cor V4
+        'v4-red': '#ED1C24',
+      }
+    }
   },
   plugins: [],
+  // opcional: força incluir estas classes mesmo se não detectadas estaticamente
+  safelist: [
+    'bg-v4-red',
+    'text-v4-red',
+    'hover:text-v4-red',
+    'focus:ring-v4-red',
+    'border-v4-red',
+    'hover:bg-red-50'
+  ],
 }
